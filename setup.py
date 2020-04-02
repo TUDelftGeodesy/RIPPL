4519cf5f340ea03ed31fb3b2173ce65f398350d0
Unknown <g.mulder-1@tudelft.nl>
2020-04-02 11:46:26 +0200

    Update required packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rippl',
-    version='3.7',
+    version='2.0',
     packages=['rippl', 'rippl.meta_data', 'rippl.resampling', 'rippl.SAR_sensors', 'rippl.SAR_sensors.radarsat',
               'rippl.SAR_sensors.sentinel', 'rippl.external_dems', 'rippl.external_dems.srtm',
               'rippl.external_dems.tandem_x', 'rippl.orbit_geometry', 'rippl.NWP_simulations',
@@ -13,6 +13,5 @@ setup(
     author='Gert Mulder',
     author_email='g.mulder-1@tudelft.nl',
     description='Radar Interferometric Parallel Processing Lab',
-    install_requires=['numpy', 'scipy', 'matplotlib', 'gdal', 'fiona', 'shapely', 'os', 'urllib', 'pickle', 'zipfile',
-                      'requests', 'datetime', 'shutil']
+    install_requires=['numpy', 'scipy', 'matplotlib', 'gdal', 'fiona', 'shapely', 'requests']
 )
