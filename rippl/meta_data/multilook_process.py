Updated fast calculation of xyz + solved bug for searching
- Fixed bug for multiple polygon in search Sentinel-1
- Incorporated fast approximate xyz coordinates using crossing sphere and circle
- Fixed bug in multilooking which removes part of image
- Added center line/pixel for images to make a better first guess of pixel location for xyz estimation
- Code to prevent swap from right to left looking in satellite image (applies for large images)

rippl/meta_data/multilook_process.py
@@ -13,8 +13,11 @@ from rippl.meta_data.image_data import ImageData
 from rippl.resampling.multilook_irregular import MultilookIrregular
 from rippl.resampling.multilook_regular import MultilookRegular
 from rippl.resampling.coor_new_extend import CoorNewExtend
+from rippl.orbit_geometry.orbit_coordinates import OrbitCoordinates
+from rippl.resampling.grid_transforms import GridTransforms
 
 
+# noinspection PyUnboundLocalVariable
 class MultilookProcess(Process):  # Change this name to the one of your processing step.
 
     def __call__(self, memory_in=True):
@@ -105,7 +108,7 @@ class MultilookProcess(Process):  # Change this name to the one of your processi
                     self.out_images[key].disk['data'][line_no: line_no + self.block_shape[0], :] = ImageData.memory2disk(data, dtype)
                 elif key in self.settings['multilooked_grids']:
                     self.ml_in_data[key] = data
-            elif key in self.settings['multilooked_grids'] or key in ['lines', 'pixels']:
+            elif key in self.settings['multilooked_grids'] or key in ['lines', 'pixels', 'incidence_angle']:
                 self.ml_in_data[key] = data
             else:
                 raise KeyError('This output type is not defined! It should be defined in the self.setting["multilooked_grids"]')
@@ -149,11 +152,9 @@ class MultilookProcess(Process):  # Change this name to the one of your processi
 
         # Check if there is a lines/pixels and the multilooking is not regular
         if not regular:
-            if (('lines' not in self.in_images.keys() or 'pixels' not in self.in_images.keys()) and not 'out_irregular_grids' in self.settings.keys()) \
-                    and 'no_pre_calculated_coordinates' not in self.settings.keys():
-                raise LookupError('There should be a lines and pixels input variable to do irregular multilooking! '
-                                  'If you do a regular multilook define self.regular = True in the __init__')
-            elif 'no_pre_calculated_coordinates' not in self.settings.keys():
+            if 'no_line_pixel_input' in self.settings.keys():
+                pass
+            elif 'no_line_pixel_input' not in self.settings.keys():
                 if 'out_irregular_grids' not in self.settings.keys():
                     lines = self.in_images['lines'].disk['data']
                     pixels = self.in_images['pixels'].disk['data']
@@ -178,6 +179,13 @@ class MultilookProcess(Process):  # Change this name to the one of your processi
             self.ml_coordinates.shape[0] = np.minimum(shape[0] - self.no_block * self.no_lines, self.no_lines)
             self.block_shape = self.ml_coordinates.shape
 
+            if not regular:
+                if 'no_line_pixel_input' in self.settings.keys():
+                    ml_lines, ml_pixels = self.calc_line_pixel()
+                else:
+                    ml_lines = np.copy(lines[line_no: line_no + self.block_shape[0], :])
+                    ml_pixels = np.copy(pixels[line_no: line_no + self.block_shape[0], :])
+
             print('Processing ' + str(self.no_block) + ' out of ' + str(self.no_blocks))
             self.before_multilook_calculations()
 
@@ -191,12 +199,7 @@ class MultilookProcess(Process):  # Change this name to the one of your processi
                 elif not regular:
                     multilook = MultilookIrregular(self.ml_coordinates, self.coordinate_systems['out_coor'])
                     line_no = self.no_block * self.no_lines
-                    if 'no_pre_calculated_coordinates' in self.settings.keys():
-                        multilook.create_conversion_grid(self['lines'], self['pixels'])
-                    else:
-                        multilook.create_conversion_grid(
-                            np.copy(lines[line_no: line_no + self.block_shape[0], :]),
-                            np.copy(pixels[line_no: line_no + self.block_shape[0], :]))
+                    multilook.create_conversion_grid(ml_lines, ml_pixels)
                     multilook.apply_multilooking(self.ml_in_data[file_type])
                     self.block_processing = False
                     self[file_type] += multilook.multilooked
@@ -215,6 +218,56 @@ class MultilookProcess(Process):  # Change this name to the one of your processi
             self[file_type][valid_pixels] /= (looks[valid_pixels] / no_types)
 
         self.after_multilook_calculations()
+
+    def calc_line_pixel(self):
+        """
+        Here the lines and pixels are calculated directly without using the prepare multilooking step.
+        This is faster for smaller stacks, but slower for large stacks.
+
+        Returns
+        -------
+
+        """
+
+        orbit = self.processing_images['slave'].find_best_orbit('original')
+        self.ml_coordinates.create_radar_lines()        # type: CoordinateSystem
+
+        orbit_coor = OrbitCoordinates(orbit=orbit, coordinates=self.ml_coordinates)
+
+        if self.coordinate_systems['out_coor'].grid_type == 'radar_coordinates':
+            if self.settings['regular']:
+                pass
+            else:
+                raise TypeError('Multilooking between two different radar systems is not supported.')
+
+        if 'dem' in self.settings.keys():
+            dem = self['dem']
+        else:
+            R, geoid_h = orbit_coor.globe_center_distance(self.ml_coordinates.center_lat, self.ml_coordinates.center_lon)
+            dem = np.ones(self.ml_coordinates.shape) * geoid_h
+
+        if self.ml_coordinates.grid_type == 'radar_coordinates':
+            orbit_coor.height = dem
+            orbit_coor.approx_lph2xyz()
+            orbit_coor.xyz2ell()
+
+            self['incidence_angle'] = np.reshape(orbit_coor.incidence, self.ml_coordinates.shape)
+            lat = np.reshape(orbit_coor.lat, self.ml_coordinates.shape)
+            lon = np.reshape(orbit_coor.lon, self.ml_coordinates.shape)
+        elif self.ml_coordinates.grid_type == 'geographic':
+            lat, lon = self.ml_coordinates.create_latlon_grid()
+        elif self.ml_coordinates.grid_type == 'projection':
+            x, y = self.ml_coordinates.create_xy_grid()
+            lat, lon = self.ml_coordinates.proj2ell(x, y)
+
+        # Final step is to calculate the lines and pixels in the final output.
+        transform = GridTransforms(self.coordinate_systems['out_coor'], self.ml_coordinates)
+        transform.add_dem(dem)
+        transform.add_lat_lon(lat, lon)
+
+        lines, pixels = transform()
+
+        return lines, pixels
 
     def before_multilook_calculations(self):
         """

rippl/processing_steps/geocode.py
@@ -15,7 +15,7 @@ from rippl.orbit_geometry.orbit_coordinates import OrbitCoordinates
 
 class Geocode(Process):  # Change this name to the one of your processing step.
 
-    def __init__(self, data_id='', out_coor=[], coreg_master='coreg_master', overwrite=False):
+    def __init__(self, data_id='', out_coor=[], coreg_master='coreg_master', overwrite=False, approximate=False):
 
         """
         :param str data_id: Data ID of image. Only used in specific cases where the processing chain contains 2 times
@@ -56,6 +56,7 @@ class Geocode(Process):  # Change this name to the one of your processing step.
         self.processing_images = dict()
         self.processing_images['coreg_master'] = coreg_master
         self.settings = dict()
+        self.settings['approximate'] = approximate
 
     def init_super(self):
 
@@ -87,7 +88,10 @@ class Geocode(Process):  # Change this name to the one of your processing step.
             orbit_interp.lp_time()
 
             # Calculate cartesian coordinates
-            orbit_interp.lph2xyz()
+            if self.settings['approximate']:
+                orbit_interp.approx_lph2xyz()
+            else:
+                orbit_interp.lph2xyz()
             self['X'] = np.reshape(orbit_interp.xyz[0, :], self.block_coor.shape)
             self['Y'] = np.reshape(orbit_interp.xyz[1, :], self.block_coor.shape)
             self['Z'] = np.reshape(orbit_interp.xyz[2, :], self.block_coor.shape)

rippl/resampling/coor_concatenate.py
@@ -56,6 +56,11 @@ class CoorConcatenate():
         readfile.json_dict['Orig_first_pixel'] = 'None'
         readfile.first_pixel = self.concat_coor.first_pixel
         readfile.first_line = self.concat_coor.first_line
+        readfile.center_lat = self.concat_coor.center_lat
+        readfile.center_lon = self.concat_coor.center_lon
+        readfile.center_line = self.concat_coor.center_line
+        readfile.center_pixel = self.concat_coor.center_pixel
+        readfile.center_heading = self.concat_coor.center_heading
 
         # Get the oblique mercator projection
         orbit_calculations = OrbitCoordinates(orbit=orbit)
@@ -74,9 +79,7 @@ class CoorConcatenate():
                               coor.proj2ell(y_coor[0], x_coor[1]), coor.proj2ell(y_coor[0], x_coor[0])]
         readfile.size = [self.concat_coor.shape[0] + self.concat_coor.first_line,
                          self.concat_coor.shape[1] + self.concat_coor.first_pixel]
-        readfile.center_lat = self.concat_coor.center_lat
-        readfile.center_lon = self.concat_coor.center_lon
-        readfile.center_heading = self.concat_coor.center_heading
+
 
         # Then remove all irrelevant parts of the new readfiles image (no link to source data or something)
         for key in ['First_line (w.r.t. tiff_image)', 'Last_line (w.r.t. tiff_image)',
@@ -191,6 +194,8 @@ class CoorConcatenate():
         concat_coor.center_lat = np.mean([coor.center_lat for coor in coor_systems])
         concat_coor.center_lon = np.mean([coor.center_lon for coor in coor_systems])
         concat_coor.center_heading = np.mean([coor.center_heading for coor in coor_systems])
+        concat_coor.center_pixel = np.int(np.mean([coor.center_pixel for coor in coor_systems]))
+        concat_coor.center_line = np.int(np.mean([coor.center_line for coor in coor_systems]))
 
         return concat_coor, sync_coors
 